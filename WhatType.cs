af74de43bf4329f5634677151ca621e3ddf5e2b6
EdinaelsonGaleno <sedinaelson@gmail.com>
2023-08-15 20:51:06 -0300

    Excel services

---
+++ WhatType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataExcel
+{
+    class WhatType
+    {
+        public static bool IsInt(string value)
+        {
+            bool result = int.TryParse(value, out _);
+           
+            return result;
+        }
+
+        public static bool IsFloat(string value) 
+        {
+            bool result = float.TryParse(value, out _);
+            return result;
+        }
+
+    }
+}
